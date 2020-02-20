c7f423fa1bce124c37439592fd6162474a9b2a40
Jeronimo <jeronimomora@berkeley.edu>
2020-02-19 22:06:18 -0800

    Add missed files

---
+++ src/staticData/degrees.ts
@@ -0,0 +1,15 @@
+const msMechanicalEngineering = {
+	title: 'M.S. Mechanical Engineering',
+	institution: 'UC Berkeley',
+	city: "Berkeley, CA",
+	date: "September 2016 - May 2018",
+}
+
+const bsMechanicalEngineering = {
+	title: 'B.S. Mechanical Engineering',
+	institution: 'UC Davis',
+	city: 'Davis, CA',
+	date: 'October 2015 - June 2016',
+}
+
+export default Object.freeze([msMechanicalEngineering, bsMechanicalEngineering])
